52372fb5b56776638084df9177147cc83679ab18
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2021-07-04 22:56:18 +0200

    WIP

--- .config/functions.sh
+++ .config/functions.sh
@@ -12,21 +12,21 @@ usage() {
     success "-> $(basename $0) <command> --help"
     echo
     warn "Commands:"
-    warn "start"
-    info "-> Uses 'docker-compose up -d --force-recreate' to start the server."
+    warn "start [<parameters>]"
     info "-> If 'start' is used without parameters, the server is started with the globally defined settings from the '.env' file."
     info "-> If parameters are set, these will override the global settings. The phpmyadmin and mailhog containers always start."
     echo
     warn "restart"
-    info "-> Uses 'docker-lamp halt && docker-lamp start [<parameters>]' to restart the server."
+    info "-> Restarts the server with the same configuration as started before."
+    echo
+    warn "shutdown [<parameters>]"
+    info "-> Shuts down the server completely deleting also all volumes created."
+    info "-> If not other set, all databases will be saved before."
     echo
     warn "stop"
-    info "-> Uses 'docker-compose down' to stop the server."
-    echo
-    warn "halt"
-    info "-> Uses 'docker-compose down' to stop the server."
-    info "-> However, unlike 'docker-lamp stop', the database volume is preserved and the databases are not backed up."
-    info "-> Likewise, the files in the initDB folder are ignored by the next start."
+    info "-> Stops the server."
+    info "-> However, unlike 'shutdown', the database volume is preserved and the databases are not saved before."
+    info "-> Likewise, the files in the 'initDB' folder are ignored by the next start."
     echo
     warn "update-images"
     info "-> Downloads the newest images globaly defined in the '.env' file, or set in the parameters."
@@ -38,7 +38,7 @@ usage() {
     warn "cli <CONTAINER-NAME>"
     info "-> Uses 'docker-compose down' to stop the server."
     echo
-    warn "backup-db <DB-NAME>"
+    warn "save-db <DB-NAME>"
     info "-> Uses 'docker-compose down' to stop the server."
     exit 0
 }
@@ -93,29 +93,29 @@ usage_restart() {
     headline "Help for $(basename $0) restart"
     echo
     warn "Usage:"
-    success "-> $(basename $0) restart [<parameters>]"
+    success "-> $(basename $0) restart"
     echo
-    info "-> Uses 'docker-lamp halt && docker-lamp start [<parameters>]' to restart the server."
-    echo
-    warn "For more details about the allowed parameters use:"
-    success "-> $(basename $0) start --help"
+    info "-> Restarts the server with the same configuration as started before."
     DEBUG=0
     exit 0
 }
 
-usage_stop() {
+usage_shutdown() {
     DEBUG=1
-    headline "Help for the command: stop"
+    headline "Help for the command: shutdown"
     echo
     warn "Usage:"
-    success "-> $(basename $0) stop [<parameters>]"
+    success "-> $(basename $0) shutdown [<parameters>]"
     echo
-    info "-> Uses 'docker-compose down' to shutdown the server."
-    info "-> Also all databases are saved into the folder 'initDB' and all created volumes will be deleted."
-    info_b "-> IMPORTANT: All existing files will be overridden."
+    info "-> Shuts down the server completely deleting also all volumes created."
+    info "-> If not other set, all databases will be saved before."
+    info_b "-> IMPORTANT: All existing files in 'initDB' will be overridden."
     echo
     echo
     warn "Parameters:"
+    echo
+    warn "-s, --skip-save-db"
+    info "-> If this option is set, no databases and no archives are saved."
     echo
     warn "-a, --archive"
     info "-> A copy of all databases is additionally archived in the subfolder within 'initDB' specified with the '-b or --backup-folder=' parameter."
@@ -136,20 +136,21 @@ usage_stop() {
     exit 0
 }
 
-usage_halt() {
+usage_stop() {
     DEBUG=1
-    headline "Help for the command: halt"
+    headline "Help for the command: stop"
     echo
-    warn "Usage: $(basename $0) halt [<parameters>]"
+    warn "Usage: $(basename $0) stop"
     echo
-    info "-> Uses 'docker-compose down' to shutdown the server."
-    info "-> However, unlike 'docker-lamp stop', the database volume is preserved and the databases are not backed up."
-    info "-> Likewise, the files in the initDB folder are ignored by the next start."
-    info "-> Anyway you can archive the databases."
+    info "-> Stops the server."
+    info "-> However, unlike 'shutdown', the database volume is preserved and the databases are not saved before."
+    info "-> Likewise, the files in the 'initDB' folder are ignored by the next start."
     echo
-    warn "You can still archive the databases."
+    warn "Anyway, if you like to save (and archive) the databases,"
+    warn "You need to use '$(basename $0) save-db [<parameters>]' before."
+    echo
     warn "For more details about how to, use:"
-    success "-> $(basename $0) stop --help"
+    success "-> $(basename $0) save-db --help"
     DEBUG=0
     exit 0
 }
@@ -223,6 +224,21 @@ log() {
         && info "->" "$@"
 }
 
+log_b() {
+    [ "$DEBUG" -eq 1 ] \
+        && info_b "->" "$@"
+}
+
+log_s() {
+    [ "$DEBUG" -eq 1 ] \
+        && success "->" "$@"
+}
+
+log_w() {
+    [ "$DEBUG" -eq 1 ] \
+        && warn "->" "$@"
+}
+
 add_yaml_to_load() {
     local YAML_LIST_TO_LOAD=${LOAD_YAML:-}
     local CONTAINER_PATH="$1"
@@ -238,7 +254,7 @@ add_yaml_to_load() {
         warn "and '$DOCKER_COMPOSE_BASEDIR/.config/$CONTAINER_PATH/config.yml'"
         exit 1
     fi
-    log "$CONTAINER_PATH YAML loaded."
+    log_s "$CONTAINER_PATH YAML loaded."
 }
 
 get_yaml_list() {
@@ -249,5 +265,105 @@ get_yaml_list() {
     done
 
     printf "$list"
+}
+
+start_server() {
+    create_certs \
+        && warn "Start server:" \
+        && $DOCKER_COMPOSE_CALL up -d --force-recreate \
+        && success "Server restarted."
+}
+
+restart_server() {
+    ( [ -f "$DOCKER_COMPOSE_YAML" ] && [ -z "$($DOCKER_COMPOSE_CALL ps -q)" ] ) \
+        && warn "Server is not running." && exit 0
+
+    warn "Stop server:"
+    $DOCKER_COMPOSE_CALL down \
+        && warn "Removing volumes:" \
+        && docker volume ls --filter=name=$COMPOSE_PROJECT_NAME \
+        | grep -v 'db-data-dir' | awk 'NR > 1 {print $2}' \
+        | xargs docker volume rm --force \
+        | xargs echo "Volumes removed:" \
+        && create_certs \
+        && warn "Start server:" \
+        && $DOCKER_COMPOSE_CALL up -d --force-recreate \
+        && success "Server restarted."
+}
+
+shutdown_server() {
+    ( [ -f "$DOCKER_COMPOSE_YAML" ] && [ -z "$($DOCKER_COMPOSE_CALL ps -q)" ] ) \
+        && warn "Server is not running." && exit 0
+
+    [ -z "$SKIP_SAVE_DATABASES" ] && save_db
+
+    warn "Shutdown server:" \
+        && $DOCKER_COMPOSE_CALL down -v \
+        && success "Server shut down."
+}
+
+stop_server() {
+    ( [ -f "$DOCKER_COMPOSE_YAML" ] && [ -z "$($DOCKER_COMPOSE_CALL ps -q)" ] ) \
+        && warn "Server is not running." && exit 0
+
+    warn "Stop server:"
+    $DOCKER_COMPOSE_CALL down \
+        && warn "Removing volumes:" \
+        && docker volume ls --filter=name=$COMPOSE_PROJECT_NAME \
+        | grep -v 'db-data-dir' | awk 'NR > 1 {print $2}' \
+        | xargs docker volume rm --force \
+        | xargs echo "Volumes removed:" \
+        && success "Server is stoped."
+}
+
+save_db() {
+    [ -z "$($DOCKER_COMPOSE_CALL ps -q $DATABASE_TO_USE)" ] \
+        && warn "Server '$DATABASE_TO_USE' is not running." && exit 0
+
+    local envs=""
+    [ "$ARCHIVE_DATABASES" -eq 1 ] && env="-e ARCHIVE=1 "
+    [ ! -z "$ARCHIVE_FOLDER" ] && env="${env}-e ARCHIVE_FOLDER=$ARCHIVE_FOLDER "
+
+    warn "Save databases:"
+    docker exec -it --privileged ${envs}${COMPOSE_PROJECT_NAME}_${DATABASE_TO_USE} sh -c "/usr/local/bin/backup-databases"
+}
+
+delete_obsolete_images() {
+    local OBSOLETE_IMAGES="$(docker images -f "dangling=true" -q)"
+
+    [ -z "$OBSOLETE_IMAGES" ] \
+        && success "No obsolete images found." \
+        && exit 0
+
+    warn "Found obsolete Images:"
+    info "$OBSOLETE_IMAGES"
+
+    local ERROR_DELETE_OBSOLETE="$(docker rmi $OBSOLETE_IMAGES >/dev/null 2>&1)"
+
+    [ -z "$ERROR_DELETE_OBSOLETE" ] \
+        && success "Obsolete images deleted." \
+        || error "$ERROR_DELETE_OBSOLETE"
+}
+
+create_certs() {
+    local MINICA_BASEDIR="$APP_BASEDIR/ca"
+
+    [ ! -z "$SSL_LOCALDOMAINS" ] \
+        && MINICA_DEFAULT_DOMAINS="$MINICA_DEFAULT_DOMAINS,$SSL_LOCALDOMAINS"
+
+    [ -z "$SSL_DOMAINS" ] \
+        && MINICA_DEFAULT_DOMAINS="$MINICA_DEFAULT_DOMAINS $SSL_DOMAINS"
+
+    warn "Start creating SSL certificates:"
+    for domain in $MINICA_DEFAULT_DOMAINS; do
+        docker run --user $APP_USER_ID -it --rm \
+            -v "$MINICA_BASEDIR:/certs" \
+            degobbis/minica \
+            --ca-cert minica-root-ca.pem \
+            --ca-key minica-root-ca-key.pem \
+            --domains $domain
+    done
+
+    success "All certificates created."
 }
 # find . -maxdepth 1 -type f ! -name "*.md" ! -name "*.txt"
