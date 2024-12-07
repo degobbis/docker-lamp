8c0ff1ad1aae5e22f642b34c4f5302840b1d9ff7
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2024-12-07 15:31:55 +0100

    Check for needed override folders

--- .config/functions.sh
+++ .config/functions.sh
@@ -304,8 +304,21 @@ create_gateway_and_container_ipv4() {
     DNS_B=${DNS_A//127.0.0.1/$DL_HTTPD_IPv4}
 }
 
+check_override_folders() {
+    local needed=("ca" "httpd/apache24" "initDB" "php/php56" "php/php74" "php/php80" "php/php81" "php/php82" "php/php83" "php/php84" )
+
+    for folder in "${needed[@]}"; do
+        if ! test -d "${APP_BASEDIR}/${folder}"; then
+            success "Create: ${APP_BASEDIR}/${folder}"
+            mkdir -p ${APP_BASEDIR}/${folder}
+        fi
+    done
+}
+
 start_server() {
     local _db_volume_exist=$(docker volume ls --filter=name=$COMPOSE_PROJECT_NAME | grep "${COMPOSE_PROJECT_NAME}_db-data-dir")
+
+    check_override_folders
 
     #if [ "$USE_BIND" -eq 1 ]; then
         create_certs
