71856f27dcd6c43c8e419adf09f8ce1332456d5b
Guido De Gobbis <10517822+degobbis@users.noreply.github.com>
2025-12-02 17:58:44 +0100

    Added a hook before start

---
+++ data/hooks/pre_start.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# The code of this script will be executed before the start.
+# You can override each function by define it yourself using the same function name (except for 'usage').
